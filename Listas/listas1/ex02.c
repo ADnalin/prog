d2cd99859d405730bb034515fcfbca05aa27b0c0
ADnalin <nalin.ferreira@escola.pr.gov.br>
2026-06-02 11:51:49 +0000

    ex 2 pra fazer

---
+++ Listas/listas1/ex02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef struct{
+    char timeA[50];
+    char timeB[50];
+    int golA;
+    int golB;
+}placar;
+int main(){
+    placar P;
+    printf("digite timeA: ");
+    scanf("%s", p.timeA);
+    
+    printf("\ndigite timeB: ");
+    scanf("%s", p.timeB);
+    printf("\ndigite gol time A: ");
+    scanf("%d", &p.golA);
+    printf("\ndigite gol time B");
+    scanf("%d", &p.golB);
+}
